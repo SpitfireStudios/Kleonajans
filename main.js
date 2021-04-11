7005fd2d74a0e00cc04238d8197d57a33f9623ef
Toprak Tosun <toprak.tosun@me.com>
2021-04-12 01:23:48 +0300

    commit on main

--- main.js
+++ main.js
@@ -1,30 +1,73 @@
-var rellax = new Rellax('.h1, .h2');
 
 
-window.onscroll = function() {parallax()};
 
-function parallax() {
 
-  var i;
-  var h2 = document.getElementsByClassName("h2");
-  var h1 = document.getElementsByClassName("h1");
-  var ScrollTop = document.documentElement.scrollTop;
 
-  for (i=0; i < 2; i++)
-    {
-        if (ScrollTop > 0) {
-            h1[i].style.opacity = ScrollTop/900;
-        } else {
-            h1[i].style.opacity = "0";
-        }
+function scrollDistance (callback, refresh = 66) {
+
+	// Make sure a valid callback was provided
+	if (!callback || typeof callback !== 'function') return;
+
+	// Variables
+	let isScrolling, start, end, distance;
+
+	// Listen for scroll events
+	window.addEventListener('scroll', function (event) {
+
+		// Set starting position
+		if (!start) {
+			start = window.pageYOffset;
+		}
+
+		// Clear our timeout throughout the scroll
+		window.clearTimeout(isScrolling);
+
+		// Set a timeout to run after scrolling ends
+		isScrolling = setTimeout(function() {
+
+			// Calculate distance
+			end = window.pageYOffset;
+			distance = end - start;
+
+			// Run the callback
+			callback(distance, start, end);
+
+			// Reset calculations
+			start = null;
+			end = null;
+			distance = null;
+
+		}, refresh);
+
+	}, false);
+
+}
+
+scrollDistance(function (distance) {
+	console.log('You travelled ' + parseInt(Math.abs(distance), 10) + 'px ' + (distance < 0 ? 'up' : 'down'));
+});
+
+
+
+
+
+
+
+window.addEventListener('scroll', function (parallax) {
+
+    var scrolled = window.scrollY;
+    var h1 = document.querySelectorAll('.h1');
+    var h2 = document.querySelectorAll('.h2');
+
+    h1[0].style.top = - (scrolled * -1.8) + 'px', h1[0].style.opacity = 1 - (scrolled * .005);
+
+    if (scrolled > 800) {
+        h1[0].style.display = 'none';
+        h2[0].style.left = distance + 'px';
+    }
+
+    else if (scrolled < 900){
+        h1[0].style.display = 'grid';
     }
     
-    for (i=0; i < 2; i++)
-    {
-        if (ScrollTop > 100) {
-            h2[i].style.transform = "translateX (500px)";
-        } else {
-            h2[i].style.transform = "translateX (0)";
-        }
-    }
-}
+});
