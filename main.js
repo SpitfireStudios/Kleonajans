09b28a332621ea627f82a03c0939bc0364d03705
Toprak Tosun <toprak.tosun@me.com>
2021-04-18 23:57:12 +0300

    commit on main

--- main.js
+++ main.js
@@ -8,7 +8,10 @@ window.addEventListener('scroll', function (parallax) {
     h1[0].style.top = - (scrolled * -1.8) + 'px'; 
 	h1[0].style.opacity = 1 - (scrolled * .005);
 	h1[0].style.display = 'grid';
+	h2[0].style.display = 'grid';
+	h2[1].style.display = 'grid';
 	h2[0].style.left = 0;
+	h2[1].style.left = 0;
 
 
     if (scrolled > 800) {
@@ -18,7 +21,19 @@ window.addEventListener('scroll', function (parallax) {
 	if (scrolled > 1000) {
 		h2[0].style.left = (- 1000 + scrolled) *3 + 'px';
 		h2[0].style.top = (- 1000 + scrolled) + 'px';
-		h2[0].style.opacity = scrolled/0.3;
+
+		h2[0].style.opacity = 1 - (h2[0].style.left/300);
+
+	}
+
+	if (scrolled > 2000) {
+		h2[1].style.left = (- 2000 + scrolled) *3 + 'px';
+		h2[1].style.top = (- 2000 + scrolled) + 'px';
+		h2[0].style.display = 'none';
+	}
+
+	if (scrolled > 2700) {
+		h2[1].style.display = 'none';
 	}
 
 });
