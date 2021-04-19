a6b5e8bc92d05f28b128bbb6a61b5f827b6a480f
Toprak Tosun <toprak.tosun@me.com>
2021-04-20 01:03:31 +0300

    commit on main

--- main.js
+++ main.js
@@ -1,34 +1,35 @@
 
-window.addEventListener('scroll', function (parallax) {
-
-    var scrolled = window.scrollY;
-    var h1 = document.querySelectorAll('.h1');
-	var h2 = document.querySelectorAll('.h2');
-
-    h1[0].style.top = - (scrolled * -1.8) + 'px'; 
-	h1[0].style.opacity = 1 - (scrolled * .005);
-	h1[0].style.display = 'grid';
-	h2[0].style.display = 'grid';
-	h2[1].style.display = 'grid';
 
 
-    if (scrolled > 800) {
-        h1[0].style.display = 'none';
-    }
 
-	if (scrolled > 1000) {
-		h2[0].style.top = (- 1000 + scrolled) + 'px';
-		h2[0].style.opacity = 1 - (scrolled * .005);
 
-	}
 
-	if (scrolled > 2000) {
-		h2[1].style.top = (- 2000 + scrolled) + 'px';
-		h2[0].style.display = 'none';
-	}
 
-	if (scrolled > 2700) {
-		h2[1].style.display = 'none';
-	}
 
-});
+
+let controller = new ScrollMagic.Controller();
+let timeline = new TimelineMax();
+var h1 = document.querySelectorAll(".h1");
+var h2 = document.querySelectorAll(".h2");
+
+timeline
+  .to (h1[0], 1, { y: 400 }, )
+  .to (h1[0], .20, { opacity: 0, }, )
+  .to (h2[0], 1, { x:600 , y: 400}, )
+  .to (h2[0], 1, { opacity: 0 }, )
+  .to (h2[1], 1, { x:400, y: 400, delay:-1 }, )
+  .to (h2[1], 1, { opacity: 0,},) ;
+
+
+
+  
+
+let scene = new ScrollMagic.Scene({
+  offset: 0,
+  duration: "300%",
+  triggerHook: 0,
+})
+
+
+  .setTween(timeline)
+  .addTo(controller);
