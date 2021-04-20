9706a433df2907704805f86a0559942df3da8239
Toprak Tosun <toprak.tosun@me.com>
2021-04-20 18:03:17 +0300

    commit on main

--- main.js
+++ main.js
@@ -15,9 +15,9 @@ var h2 = document.querySelectorAll(".h2");
 timeline
   .to (h1[0], 1, { y: 400 }, )
   .to (h1[0], .20, { opacity: 0, }, )
-  .to (h2[0], 1, { x:600 , y: 400}, )
+  .to (h2[0], 1, { x:650 , y: 200}, )
   .to (h2[0], 1, { opacity: 0 }, )
-  .to (h2[1], 1, { x:400, y: 400, delay:-1 }, )
+  .to (h2[1], 1, { x:650, y: 200, delay:-1 }, )
   .to (h2[1], 1, { opacity: 0,},) ;
 
 
