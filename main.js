0c2949ac9f86110e2dd437e9a8233e2a9bd02a5d
Toprak Tosun <toprak.tosun@me.com>
2021-05-02 19:59:40 +0300

    commit on main

--- main.js
+++ main.js
@@ -7,29 +7,36 @@
 
 
 
-let controller = new ScrollMagic.Controller();
-let timeline = new TimelineMax();
-var h1 = document.querySelectorAll(".h1");
-var h2 = document.querySelectorAll(".h2");
+var timeline = new TimelineMax();
+var timeline2 = new TimelineMax();
+var timeline3 = new TimelineMax();
+const h1 = document.querySelectorAll("h1");
+const h2 = document.querySelectorAll("h2");
 
-timeline
-  .to (h1[0], 1, { y: 400 }, )
-  .to (h1[0], .20, { opacity: 0, }, )
-  .to (h2[0], 1, { x:650 , y: 200}, )
-  .to (h2[0], 1, { opacity: 0 }, )
-  .to (h2[1], 1, { x:650, y: 200, delay:-1 }, )
-  .to (h2[1], 1, { opacity: 0,},) ;
+	var controller = new ScrollMagic.Controller( {globalSceneOptions: {triggerHook: .5}});
 
+  var tween1 = timeline.to(h1[0], {y:50, ease: Linear.easeNone, duration:1}).to(h1[0], {opacity:0})
 
+  var tween2 = timeline2.to(h2[0],{x:"35vw", ease: Linear.easeNone, duration:1}).to(h2[0], {opacity:0})
 
-  
-
-let scene = new ScrollMagic.Scene({
-  offset: 0,
-  duration: "300%",
-  triggerHook: 0,
-})
+  var tween3 = timeline3.to(h2[1],{x:"35vw", ease: Linear.easeNone, duration:1}).to(h2[1], {opacity:0})
 
 
-  .setTween(timeline)
-  .addTo(controller);
+	// build scenes
+	new ScrollMagic.Scene({triggerElement: h1[0], duration: "50%",})
+					.setTween(tween1)
+          			.setPin(h1[0])
+					.addIndicators()
+					.addTo(controller);
+
+	new ScrollMagic.Scene({triggerElement: h2[0], duration: "75%",})
+					.setTween(tween2)
+          			.setPin(h2[0])
+					.addIndicators()
+					.addTo(controller);
+
+	new ScrollMagic.Scene({triggerElement: h2[1], duration: "75%"})
+					.setTween(tween3)
+          			.setPin(h2[1])
+					.addIndicators()
+					.addTo(controller);
